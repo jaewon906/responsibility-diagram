b93dcae1cca6ebf69e159fecb2e62157a3f28204
Park Jae Won <ploi9@naver.com>
2025-01-11 19:46:26 +0900

    add provide

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,40 @@
 <template>
+    <search-filter @search="find"/>
     <diagram-page :data="dummys"/>
 </template>
 
 <script setup="init">
 import dummys from "@/dummy.js";
-import {onMounted, ref} from "vue";
+import {onMounted, provide, ref} from "vue";
 import DiagramPage from "@/components/DiagramPage.vue";
+import SearchFilter from "@/components/SearchFilter.vue";
 
+const data = ref({})
 onMounted(()=>{
   init();
 })
+provide("searchParam", data)
 
-const resData = ref([])
+// const resData = ref([])
 
 function init(){
-  makeResponsibilityDiagram(dummys)
+  // makeResponsibilityDiagram(dummys)
 }
 
-const makeResponsibilityDiagram = (data) => {
-  for(let object of data) {
-    const obj = {}
-    for (let [key, value] of Object.entries(object)) {
-      if(key !== "children")
-        obj[key] = value
-      if(key === "children" && value.length > 0)
-        makeResponsibilityDiagram(value)
-    }
-    resData.value.unshift(obj)
-  }
+// const makeResponsibilityDiagram = (data) => {
+//   for(let object of data) {
+//     const obj = {}
+//     for (let [key, value] of Object.entries(object)) {
+//       if(key !== "children")
+//         obj[key] = value
+//       if(key === "children" && value.length > 0)
+//         makeResponsibilityDiagram(value)
+//     }
+//     resData.value.unshift(obj)
+//   }
+// }
+const find = (d)=>{
+  data.value = d;
 }
 </script>
 
